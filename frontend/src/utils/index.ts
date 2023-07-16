5f770874d55e733f640fcf7f7f0d2e2b588d62ee
Rakibul Islam <rakibul.islam7772588@gmail.com>
2023-07-16 21:06:07 +0600

    deploy: added deployment settings

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,4 +1,4 @@
 export const BASE_API_URL: string = import.meta.env.VITE_BASE_API_URL as string;
 export const BASE_URL: string = import.meta.env.VITE_BASE_URL as string;
 export const NODE_ENV: string = import.meta.env.NODE_ENV as string;
-export const RESULTS_PER_PAGE = 20;
+export const RESULTS_PER_PAGE = 10;
